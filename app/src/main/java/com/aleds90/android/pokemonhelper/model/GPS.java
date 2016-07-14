478c88940cb663441759c21adf991b5b1c86a321
blennoit <alessandro.distefano1@live.it>
2016-07-14 19:08:19 +0200

    commit

---
+++ app/src/main/java/com/aleds90/android/pokemonhelper/model/GPS.java
@@ -0,0 +1,25 @@
+package com.aleds90.android.pokemonhelper.model;
+
+public class GPS {
+
+    private double latitude;
+    private double altitude;
+
+    public GPS(){}
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+}
--- app/src/main/java/com/aleds90/android/pokemonhelper/view/MainActivity.java
+++ app/src/main/java/com/aleds90/android/pokemonhelper/view/MainActivity.java
@@ -1,10 +1,18 @@
 package com.aleds90.android.pokemonhelper.view;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import com.aleds90.android.pokemonhelper.R;
+import com.aleds90.android.pokemonhelper.model.GPS;
 import com.aleds90.android.pokemonhelper.model.Gym;
 import com.aleds90.android.pokemonhelper.model.GymDAO;
 import com.aleds90.android.pokemonhelper.model.Pokemon;
@@ -13,7 +21,10 @@ import com.aleds90.android.pokemonhelper.model.PokemonDAO;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    protected LocationManager locationManager;
+    GPS gps = new GPS();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +35,28 @@ public class MainActivity extends AppCompatActivity {
 
         PokemonDAO pokemonDAO = new PokemonDAO(getApplicationContext());
         ArrayList<Pokemon> pokemonArrayList = pokemonDAO.getPokemons();
-        System.out.println("ARRAY SIZE: "+ pokemonArrayList.size());
+
 
         ListView pokemons = (ListView)findViewById(R.id.pokemons);
-        pokemons.setAdapter(new PokemonAdapter( fakeList(),getApplicationContext()));
+        pokemons.setAdapter(new PokemonAdapter( pokemonArrayList,getApplicationContext()));
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
     }
 
     private ArrayList<Pokemon> fakeList(){
+
         PokemonDAO pokemonDAO = new PokemonDAO(getApplicationContext());
         GymDAO gymDAO = new GymDAO(getApplicationContext());
 
@@ -41,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         gym1.setNotes("note1");
         gym1.setLatitude(1.0);
         gym1.setLongitude(1.0);
-        gymDAO.save(gym1);
+        gym1.setId((int)gymDAO.save(gym1));
 
         Gym gym2 = new Gym();
         gym2.setAddress("indirizzo2");
@@ -49,29 +74,33 @@ public class MainActivity extends AppCompatActivity {
         gym2.setNotes("note2");
         gym2.setLatitude(2.0);
         gym2.setLongitude(2.0);
-        gymDAO.save(gym2);
+        gym2.setId((int)gymDAO.save(gym2));
 
         Gym gym3 = new Gym();
+
         gym3.setAddress("indirizzo3");
         gym3.setLevel(3);
         gym3.setNotes("note3");
         gym3.setLatitude(3.0);
         gym3.setLongitude(3.0);
-        gymDAO.save(gym3);
+        gym3.setId((int)gymDAO.save(gym3));
 
         Pokemon pokemon1 = new Pokemon();
+        pokemon1.setId(1);
         pokemon1.setName("name1");
         pokemon1.setCp(100);
         pokemon1.setGym(gym1);
         pokemonDAO.save(pokemon1);
 
         Pokemon pokemon2 = new Pokemon();
+        pokemon1.setId(2);
         pokemon2.setName("name2");
         pokemon2.setCp(200);
         pokemon2.setGym(gym2);
         pokemonDAO.save(pokemon2);
 
         Pokemon pokemon3 = new Pokemon();
+        pokemon3.setId(3);
         pokemon3.setName("name3");
         pokemon3.setCp(300);
         pokemon3.setGym(gym3);
@@ -83,5 +112,26 @@ public class MainActivity extends AppCompatActivity {
         pokemons.add(pokemon3);
 
         return pokemons;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        gps.setAltitude(location.getAltitude());
+        gps.setLatitude(location.getLatitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
